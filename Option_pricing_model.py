1bd090624c22c333a80ef4eb24ffbdd891bb1652
ChiragArora31 <76108151+ChiragArora31@users.noreply.github.com>
2023-09-03 11:59:19 +0530

    Update Option_pricing_model.py

--- Option_pricing_model.py
+++ Option_pricing_model.py
@@ -14,13 +14,15 @@ def calculate_option_price(N, S, X, u, d, r):
     option_price = option_prices[0]
     return round(option_price, 3)
 st.title("Binomial Option Pricing Calculator")
+N_enter = st.number_input("Enter the Number of Periods (N):", min_value=1, max_value=500, step=1)
+N = st.slider("Select the Number of Periods (N):", min_value=1, max_value=500, step=1, value=N_enter)
 
 N = N = st.slider("Select the Number of Periods (N):", min_value=1, max_value=30, step=1)
 S = st.number_input("Enter the Current Market Price of the Asset (S):", min_value=0.00, step=0.01)
 X = st.number_input("Enter the Strike Price of the Asset (X):", min_value=0.00, step=0.01)
-u = st.number_input("Enter the Up-factor (u):", min_value=0.00, step=0.01)
-d = st.number_input("Enter the Down-factor (d):", min_value=0.00, step=0.01)
-r = st.number_input("Enter the Risk Free Interest Rate per Period (r) in %:", min_value=0.00, step=0.01) / 100
+u = st.number_input("Enter the Up-factor (u):", min_value=0.000, step=0.001)
+d = st.number_input("Enter the Down-factor (d):", min_value=0.000, step=0.001)
+r = st.number_input("Enter the Risk Free Interest Rate per Period (r) in %:", min_value=-100.00, step=0.01) / 100
 
 if st.button("Calculate"):
     option_price = calculate_option_price(N, S, X, u, d, r)
