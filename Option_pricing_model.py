272f25eeb961374dda99d07cfb61595a5ac25b95
ChiragArora31 <76108151+ChiragArora31@users.noreply.github.com>
2023-09-13 22:51:40 +0530

    Update Option_pricing_model.py

--- Option_pricing_model.py
+++ Option_pricing_model.py
@@ -1,11 +1,14 @@
 import streamlit as st
 
-def calculate_option_price(N, S, X, u, d, r):
+def calculate_option_price(N, S, X, u, d, r, option_type):
     p = round((1 + r - d) / (u - d), 3)
     
     option_prices = [0] * (N + 1)
     for k in range(N + 1):
-        option_prices[k] = max(S * (u ** k) * (d ** (N - k)) - X, 0)
+        if option_type == 'call':
+            option_prices[k] = max(S * (u ** k) * (d ** (N - k)) - X, 0)
+        elif option_type == 'put':
+            option_prices[k] = max(X - S * (u ** k) * (d ** (N - k)), 0)
 
     for i in range(N - 1, -1, -1):
         for k in range(i + 1):
@@ -13,19 +16,21 @@ def calculate_option_price(N, S, X, u, d, r):
 
     option_price = option_prices[0]
     return round(option_price, 3)
-st.title("Binomial Option Pricing Calculator")
-N_enter = st.number_input("Enter the Number of Periods (N):", min_value=1, max_value=500, step=1)
-N = st.slider("Select the Number of Periods (N):", min_value=1, max_value=500, step=1, value=N_enter)
-if N != N_enter:
-    N_enter = N
 
-S = st.number_input("Enter the Current Market Price of the Asset (S):", min_value=0.00, step=0.01)
-X = st.number_input("Enter the Strike Price of the Asset (X):", min_value=0.00, step=0.01)
+st.title("Binomial Option Pricing Calculator")
+option_type = st.radio("Select Option Type:", ('Call', 'Put'))
+
+N_enter = st.number_input("Enter the Number of Periods (N):", min_value=1, max_value=1000, step=1)
+
+S = st.number_input("Enter the Current Market Price of the Asset (S):", min_value=0.00, step=0.01, format="%.4f")
+X = st.number_input("Enter the Strike Price of the Asset (X):", min_value=0.00, step=0.01, format="%.4f")
 u = st.number_input("Enter the Up-factor (u):", min_value=0.000, step=0.001)
 d = st.number_input("Enter the Down-factor (d):", min_value=0.000, step=0.001)
-r = st.number_input("Enter the Risk Free Interest Rate per Period (r) in %:", min_value=-100.00, step=0.01) / 100
+r = st.number_input("Enter the Risk Free Interest Rate per Period (r) in %:", step=0.0001, format="%.4f") / 100
 
 if st.button("Calculate"):
-    option_price = calculate_option_price(N, S, X, u, d, r)
-    st.write(f"The European option price at time T=0 is: {option_price}")
-
+    option_price = calculate_option_price(N_enter, S, X, u, d, r, option_type)
+    if option_type == 'call':
+        st.write(f"The European call option price at time T=0 is: {option_price}")
+    elif option_type == 'put':
+        st.write(f"The European put option price at time T=0 is: {option_price}")
